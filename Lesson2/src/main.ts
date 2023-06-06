f5f170171ec7cd8bb1cdd64a1773dfee7e9ffb9c
Leonel Gonzalez <16leog@gmail.com>
2023-06-06 16:20:43 -0500

    Learned how to develop a typescript todo list app

--- Lesson2/src/main.ts
+++ Lesson2/src/main.ts
@@ -1,1 +1,19 @@
-let myName: string = 'Leo'
+let myName: string = 'Leo' // typescript infers 
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = 'John'
+meaningOfLife = 42
+isLoading = true
+album = 5150
+
+// infers return type
+const sum = (a: number, b: string) => {
+    return a+b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
